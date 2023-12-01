508ea57bb3499326fb32c8205c55f79a7b68c9fd
DR0698DL <dylan-externe.regnault@enedis.fr>
2023-12-01 13:54:33 +0100

    Day 1 part 2

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,33 +1,59 @@
 const fs = require('fs');
 
 let initialData;
-const number = ["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"]
-// Use fs.readFile() method to read the file 
+const numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+ 
 fs.readFile('./data.txt', 'utf8', function(err, data){ 
     initialData = data.split(/\r?\n|\r|\n/g);
 
     let totalValue = 0;
     initialData.forEach((lineOfdata, index) => {
         let lineValue = ""
-        let isFinish = false;
-        for (let index = 0; index < lineOfdata.length; index++) {
-            const character = lineOfdata[index]
-            if (Number.isInteger(Number(character)) && isFinish === false) {
-                lineValue += character;
-                isFinish = true;
-            }
+        let pattern = new RegExp(/one|two|three|four|five|six|seven|eight|nine|[0-9]/, 'g')
+        pattern.test(lineOfdata)
+        const firstValueIndex = lineOfdata.search(pattern);
+        const firstValueLastIndex = pattern.lastIndex
+        
+        pattern = new RegExp(/one|two|three|four|five|six|seven|eight|nine|[0-9]/, 'g')
+        const [lastValueIndex, lastValueLastIndex] = regexLastIndexOf(lineOfdata, pattern)
+        
+        if (Number.isInteger(Number(lineOfdata[firstValueIndex]))) {
+            lineValue += lineOfdata[firstValueIndex]
+        } else {
+            lineValue += String(numbers[lineOfdata.substring(firstValueIndex, firstValueLastIndex)])
         }
-        isFinish = false;
-        for (let index = lineOfdata.length - 1; index >= 0; index--) {
-            const character = lineOfdata[index]
-            if (Number.isInteger(Number(character)) && isFinish === false) {
-                lineValue += character;
-                isFinish = true;
-                
-            }
+        
+        if (Number.isInteger(Number(lineOfdata[lastValueIndex]))) {
+            lineValue += lineOfdata[lastValueIndex]
+        } else {
+            lineValue += String(numbers[lineOfdata.substring(lastValueIndex, lastValueLastIndex)])
         }
-        totalValue += Number(lineValue)
+
+        if (Number.isNaN(Number(lineValue))) {
+            console.log(index)
+        }
+        totalValue += Number(lineValue);
     });
 
     console.log(totalValue)
 });
+
+function regexLastIndexOf(string, regex, startpos) {
+    regex = (regex.global) ? regex : new RegExp(regex.source, "g" + (regex.ignoreCase ? "i" : "") + (regex.multiLine ? "m" : ""));
+    if(typeof (startpos) == "undefined") {
+        startpos = string.length;
+    } else if(startpos < 0) {
+        startpos = 0;
+    }
+    var stringToWorkWith = string.substring(0, startpos + 1);
+    var lastIndexOf = -1;
+    var lastIndexOfEnd = -1;
+    var nextStop = 0;
+    var result;
+    while((result = regex.exec(stringToWorkWith)) != null) {
+        lastIndexOf = result.index;
+        lastIndexOfEnd = result.index + result[0].length
+        regex.lastIndex = ++nextStop;
+    }
+    return [ lastIndexOf, lastIndexOfEnd ];
+}
