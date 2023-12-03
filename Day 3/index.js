33aba6d86d61373b2056c6aeae8064fb47514a72
Stomias <dylan.regnault2000@gmail.com>
2023-12-03 11:17:31 +0100

    Day 3: part 2

--- Day 3/index.js
+++ Day 3/index.js
@@ -5,79 +5,55 @@ fs.readFile('./data.txt', 'utf8', function(err, data){
 
     sumOfAll = 0;
     initialData.forEach((line, index) => {
-        const regex = new RegExp(/[0-9]+/, "g")
+        const regex = new RegExp(/[\*]+/, "g")
         while ((match = regex.exec(line)) != null) {
-            if (isThereNeighborAroundNumber(initialData, index, match.index, match.index + match[0].length - 1)) {
-                sumOfAll += Number(match[0])
+            const allNumbers = getNumbers(initialData, index, match.index)
+            if (allNumbers.length > 0) {
+                sumOfAll += allNumbers[0] * allNumbers[1]
             }
         }
     })
     console.log(sumOfAll)
 })
 
-const isThereNeighborAroundNumber = (initialData, rowIndex, charIndexBegin, charIndexEnd) => {
-    let hasCharacter
-    for (let index = charIndexBegin; index <= charIndexEnd; index++) {
-        hasCharacter = getNeighbors(initialData, rowIndex, index)
-        if (hasCharacter) {
-            return hasCharacter
-        }
-    }
-    return hasCharacter
-}
-
-function getNeighbors(schematic, row, col) {
+function getNumbers(schematic, row, col) {
+    let totalMatch = []
     for (let iRow = row - 1; iRow <= row + 1; iRow++) {
-      for (let iCol = col - 1; iCol <= col + 1; iCol++) {
-        if (iRow >= 0 && iRow < schematic.length && iCol >= 0 && iCol < schematic[iRow].length) {
-          if (schematic[iRow][iCol].match(/[^0-9.]/)) {
-            return true
+        for (let iCol = col - 1; iCol <= col + 1; iCol++) {
+          if (iRow >= 0 && iRow < schematic.length && iCol >= 0 && iCol < schematic[iRow].length) {
+            if (schematic[iRow][iCol].match(/[0-9]/)) {
+              const fullNumber = findFullNumber(schematic, iRow, iCol);
+              if (!totalMatch.includes(fullNumber)) {
+                totalMatch.push(fullNumber)
+              }
+            }
           }
         }
       }
-    }
-    return false;
-  }
+      if (totalMatch.length === 2) {
+        return totalMatch;
+      } else {
+        return []
+      }
+}
 
-const findCharacterAround = (initialData, indexOfLine, indexOfCharacter) => {
-    // Ligne au dessus
-    if (indexOfLine > 0) {
-        // top left
-        if (indexOfCharacter > 0 && Number.isInteger(Number(initialData[indexOfLine - 1][indexOfCharacter - 1]))) {
-            console.log("top left")
-        }
-        // top middle
-        if (Number.isInteger(Number(initialData[indexOfLine - 1][indexOfCharacter]))) {
-            console.log("top middle")
-        }
-        // top right
-        if (Number.isInteger(Number(initialData[indexOfLine - 1][indexOfCharacter + 1]))) {
-            console.log("top right")
+function findFullNumber(schematic, row, col) {
+    let completeNumber = "";
+    // d'abord à gauche
+    for (let index = col; index >= col - 2; index--) {
+        if (!Number.isNaN(Number(schematic[row][index]))) {
+            completeNumber = schematic[row][index] + completeNumber;
+        } else {
+            break;
         }
     }
-    // Ligne au milieu
-        // left
-        if (indexOfCharacter > 0 && Number.isInteger(Number(initialData[indexOfLine][indexOfCharacter - 1]))) {
-            console.log("left")
-        }
-        // right
-        if (indexOfCharacter < initialData.length - 1 &&indexOfCharacter < initialData.length - 1 &&Number.isInteger(Number(initialData[indexOfLine][indexOfCharacter + 1]))) {
-            console.log("right")
-        }
-
-    // Ligne du bas
-    if (indexOfLine < initialData.length - 1) {
-        // bottom left
-        if (indexOfCharacter > 0 && Number.isInteger(Number(initialData[indexOfLine - 1][indexOfCharacter - 1]))) {
-            console.log("bottom left")
-        }
-        // bottom middle
-        if (Number.isInteger(Number(initialData[indexOfLine - 1][indexOfCharacter]))) {
-            console.log("bottom middle")
-        }
-        // bottom right
-        if (indexOfCharacter < initialData.length - 1 && Number.isInteger(Number(initialData[indexOfLine - 1][indexOfCharacter + 1]))) {
-            console.log("bottom right")
+    // puis à droite
+    for (let index = col + 1; index <= col + 2; index++) {
+        if (!Number.isNaN(Number(schematic[row][index]))) {
+            completeNumber = completeNumber + schematic[row][index];
+        } else {
+            break;
         }
     }
+    return completeNumber;
 }
